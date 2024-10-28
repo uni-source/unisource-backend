5e3417a18ce655af6e4ad72b6b30f7f464403030
Reshan Gayantha <reshangayantha9@gmail.com>
2024-10-28 19:00:23 +0530

    Config all deployments

--- identity-service/src/test/java/com/UniSource/identity_service/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/com/UniSource/identity_service/IdentityServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IdentityServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
